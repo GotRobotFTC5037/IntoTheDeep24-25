962280fb1b29a5ca49b1627d29ad5d0705da48a6
GR5037 <153047129+GR5037@users.noreply.github.com>
2024-08-27 19:29:53 -0500

    Changed Tele name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OurTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OurTeleOp.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-@TeleOp(name = "Concept: NullOp", group = "Concept")
+
+
+@TeleOp(name = "OurTeleOp", group = "Concept")
 public class OurTeleOp extends OpMode {
     @Override
     public void init() {
