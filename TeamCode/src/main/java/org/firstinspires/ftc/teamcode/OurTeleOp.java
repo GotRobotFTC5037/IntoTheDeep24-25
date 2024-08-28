6a7b79268f71ed2e69f8ee95c39f88acb1d01b05
Luke <bennettcat12@gmail.com>
2024-08-27 20:32:38 -0500

    Added gamepad controls to Tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OurTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OurTeleOp.java
@@ -2,15 +2,41 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 @TeleOp(name = "Concept: NullOp", group = "Concept")
 public class OurTeleOp extends OpMode {
+
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
+
     @Override
     public void init() {
+
+        frontLeft = hardwareMap.dcMotor.get("fl");
+        frontRight = hardwareMap.dcMotor.get("fr");
+        backLeft = hardwareMap.dcMotor.get("bl");
+        backRight = hardwareMap.dcMotor.get("br");
+
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
     }
 
     @Override
     public void loop() {
 
+        frontLeft.setPower(-gamepad1.left_stick_y + gamepad1.left_stick_x);
+        frontRight.setPower(-gamepad1.left_stick_y - gamepad1.left_stick_x);
+        backLeft.setPower(-gamepad1.left_stick_y - gamepad1.left_stick_x);
+        backRight.setPower(-gamepad1.left_stick_y + gamepad1.left_stick_x);
+
+        telemetry.addData("Stick X:", gamepad1.left_stick_x);
+        telemetry.addData("Stick Y:", gamepad1.left_stick_y);
+
+        telemetry.update();
     }
 }
